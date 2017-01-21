1431a854c8906bd63fc801505ef6ff56f69f2d73
Anthony Gakins <Anthony Gakins>
2017-01-21 10:30:05 -0500

    a

---
+++ source/components/Banner.react.js
@@ -0,0 +1,24 @@
+const React = require('react');
+const Banner  = React.createClass({
+
+//-------default API------------------
+
+// getDefaultProps: function(){},
+//getInitialState: function(){},
+// componentWillMount: function(){},
+// componentDidMount: function(){},
+//componentWillReceiveProps: function(){},
+//shouldComponentUpdate: function(){},
+// componentWillUpdate: function(){},
+
+render: function() {
+  return (
+    <div>Banner</div>
+  )
+
+},
+// componetDidUpdate: function(){},
+// compoentWillUnmount: function(){}
+});
+
+module.exports = Banner
---
+++ source/components/Section.react.js
@@ -0,0 +1,24 @@
+const React = require('react');
+const Section = React.createClass({
+
+//-------default API------------------
+
+// getDefaultProps: function(){},
+//getInitialState: function(){},
+// componentWillMount: function(){},
+// componentDidMount: function(){},
+//componentWillReceiveProps: function(){},
+//shouldComponentUpdate: function(){},
+// componentWillUpdate: function(){},
+
+render: function() {
+  return (
+    <div>Section</div>
+  )
+
+},
+// componetDidUpdate: function(){},
+// compoentWillUnmount: function(){}
+});
+
+module.exports = Section
